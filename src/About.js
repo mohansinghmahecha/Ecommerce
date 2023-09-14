3bf278f6367b32babbc48b0f7a31899d7ab74759
Mohan Singh Mahecha <mohansingh1925128@gmail.com>
2023-09-14 21:00:48 +0530

    mohan singh added

--- src/About.js
+++ src/About.js
@@ -2,9 +2,11 @@ import React from 'react'
 import HeroSection from './components/HeroSection'
 
 export default () => {
+
   const namepass = {
-    name : "thapa ecommerce",
+    name : "About Pages",
   };
+  
   return (
     <HeroSection myData={namepass} />
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -29,7 +29,7 @@ const Footer = () => {
         <footer>
         <div className="container grid grid-four-column">
             <div className="footer-about">
-              <h3>Thapa Technical</h3>
+              <h3> Mohan singh mahecha </h3>
               <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
             </div>
             <div className="footer-subscribe">
